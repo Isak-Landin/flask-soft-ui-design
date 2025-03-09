8550caaf139b23782a95161e7f1d21739e11bf69
Isak-Landin <utbisalan@gmail.com>
2025-03-09 12:21:43 +0100

    Automated commit

--- apps/static/dashboard_assets/js/demo.js
+++ apps/static/dashboard_assets/js/demo.js
@@ -192,8 +192,8 @@ Circles.create({
 //Notify
 $.notify({
 	icon: 'icon-bell',
-	title: 'Kaiadmin',
-	message: 'Premium Bootstrap 5 Admin Dashboard',
+	title: 'Isak Landin',
+	message: 'Template under development',
 },{
 	type: 'secondary',
 	placement: {
